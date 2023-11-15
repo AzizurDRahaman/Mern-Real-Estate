828a2718fcc27af2d9600a37e31f8c924d69e374
Azizur <jameshowlatt2@gmail.com>
2023-11-15 09:36:59 +0530

    Created Pages And Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
 
 export default function App() {
   return (
-    <div>
-
-    <h1 className='text-red-500'>
-      App hola
-    </h1>
-    <h2>Hola</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
