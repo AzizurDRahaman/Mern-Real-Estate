f520c7b794e3641ebac48abdf8597545822318f4
Azizur <jameshowlatt2@gmail.com>
2023-11-15 09:17:59 +0530

    Installed Reactjs and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+
+    <h1 className='text-red-500'>
+      App hola
+    </h1>
+    <h2>Hola</h2>
+    </div>
+  )
+}
